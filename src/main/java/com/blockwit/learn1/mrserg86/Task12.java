a6f980727596750594d7c21cb52f3d5794b0e65a
mrserg86 <skrylov@blockwit.io>
2022-01-13 13:52:08 +0300

    Task 12 finished

---
+++ src/main/java/com/blockwit/learn1/mrserg86/Task12.java
@@ -0,0 +1,10 @@
+package com.blockwit.learn1.mrserg86;
+
+    public class Task12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
+        }
+
+    }
